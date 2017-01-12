ff3d0bad8e88d9d9e62f66c093a12ecd7ad07c82
DESKTOP-34IU9SS\Tal <talbenasuli@gmail.com>
2017-01-12 20:19:17 +0200

    Creating timing transfer from splash activity.

--- app/src/main/java/pickit/com/pickit/SplashActivity.java
+++ app/src/main/java/pickit/com/pickit/SplashActivity.java
@@ -1,13 +1,30 @@
 package pickit.com.pickit;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private int delayTime = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        moveToNextActivity();
+    }
+
+    //Moving to the next activity after delayTime.
+    private void moveToNextActivity(){
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            //This is the function that happens after delayTime.
+            public void run() {
+
+            }
+        }, delayTime);
     }
 }
