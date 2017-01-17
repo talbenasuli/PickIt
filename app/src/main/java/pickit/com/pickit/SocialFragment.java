5da12c49a53d96b7a39fb59b269d9443c54b3f57
ASUS\yottam <pfyotam@gmail.com>
2017-01-17 20:32:10 +0200

    creating the social fragment shield

---
+++ app/src/main/java/pickit/com/pickit/SocialFragment.java
@@ -0,0 +1,40 @@
+package pickit.com.pickit;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by yottam on 17-01-17.
+ */
+
+public class SocialFragment extends Fragment {
+    public static final String TAG = "SocialFragment";
+
+    public static SocialFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        SocialFragment fragment = new SocialFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.from(getContext()).inflate(R.layout.fragment_social, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+
+}
