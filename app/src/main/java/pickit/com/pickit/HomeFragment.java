4652ef4033314c43c0e5112c592c902f26fac9a3
or <menesh2@gmail.com>
2017-01-17 20:04:27 +0200

    creating nav bar shield and home fragment

---
+++ app/src/main/java/pickit/com/pickit/HomeFragment.java
@@ -0,0 +1,40 @@
+package pickit.com.pickit;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by or on 17/01/2017.
+ */
+
+public class HomeFragment extends Fragment {
+
+    public static final String TAG = "HomeFragment";
+
+    public static HomeFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        HomeFragment fragment = new HomeFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       View view = inflater.from(getContext()).inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
--- app/src/main/java/pickit/com/pickit/MainActivity.java
+++ app/src/main/java/pickit/com/pickit/MainActivity.java
@@ -1,18 +1,23 @@
 package pickit.com.pickit;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 
 /**
  * Created by or on 12/01/2017.
  */
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //load fragment
+        getSupportFragmentManager().beginTransaction()
+                                    .replace(R.id.fragmets_container, HomeFragment.newInstance(), HomeFragment.TAG)
+                                    .commit();
     }
 }
