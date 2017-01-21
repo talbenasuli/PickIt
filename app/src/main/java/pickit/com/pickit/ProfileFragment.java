98f4d80cfa2e135987963a886ae98085294aeecf
DESKTOP-34IU9SS\Tal <talbenasuli@gmail.com>
2017-01-21 19:47:42 +0200

    create Profile Fragment

---
+++ app/src/main/java/pickit/com/pickit/ProfileFragment.java
@@ -0,0 +1,37 @@
+package pickit.com.pickit;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ProfileFragment extends Fragment {
+    public static final String TAG = "ProfileFragment";
+
+    public static ProfileFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        ProfileFragment fragment = new ProfileFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.from(getContext()).inflate(R.layout.fragment_profile, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
