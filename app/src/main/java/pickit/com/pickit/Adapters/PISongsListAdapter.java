495e9ebca3603b9431f4fa1287ceaffb7adcc9f7
DESKTOP-34IU9SS\Tal <talbenasuli@gmail.com>
2017-03-03 13:49:02 +0200

    added comments and arranged the custome row of the songs table

--- app/src/main/java/pickit/com/pickit/Adapters/PISongsListAdapter.java
+++ app/src/main/java/pickit/com/pickit/Adapters/PISongsListAdapter.java
@@ -23,18 +23,30 @@ import pickit.com.pickit.R;
 
 public class PISongsListAdapter extends ArrayAdapter<PISong> {
 
+    /**
+     * Holds the layout components after inflation
+     * this class stints the times of the searching and inflating actions,
+     * after preforming those actions once it wont be repeated again.
+     */
     private static class ViewHolder{
         private ImageView image;
         private TextView songName;
         private TextView authorName;
         private ImageButton pickitButton;
         private TextView pickitsCounter;
+        private TextView position;
     }
 
+    //Parameters:
     List<PISong> songsList ;
     private Context context;
     private int resource;
 
+    /**
+     *
+     * @param context the context of the system.
+     * @param resource the id of the layout that we want to present.
+     */
     public PISongsListAdapter(Context context, int resource) {
         super(context, resource);
         this.context = context;
@@ -46,12 +58,15 @@ public class PISongsListAdapter extends ArrayAdapter<PISong> {
         return songsList.size();
     }
 
-    public void setSongsList(List<PISong> songsList){
+    public void setSongsList(List<PISong> songsList) {
         this.songsList = songsList;
     }
 
     @Nullable
     @Override
+    /**
+     * @param postion the index of the row.
+     */
     public PISong getItem(int position) {
         return songsList.get(position);
     }
@@ -61,7 +76,7 @@ public class PISongsListAdapter extends ArrayAdapter<PISong> {
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder viewHolder = null;
 
-        if(convertView == null){
+        if(convertView == null) {
             LayoutInflater inflater = LayoutInflater.from(context);
             convertView = inflater.inflate(resource, parent, false);
             viewHolder = new ViewHolder();
@@ -70,6 +85,7 @@ public class PISongsListAdapter extends ArrayAdapter<PISong> {
             viewHolder.authorName = (TextView) convertView.findViewById(R.id.authorNameTextView);
             viewHolder.pickitButton = (ImageButton) convertView.findViewById(R.id.pickitButton);
             viewHolder.pickitsCounter = (TextView) convertView.findViewById(R.id.pickitCounterTextView);
+            viewHolder.position = (TextView) convertView.findViewById(R.id.position);
             convertView.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolder) convertView.getTag();
@@ -79,6 +95,7 @@ public class PISongsListAdapter extends ArrayAdapter<PISong> {
         viewHolder.songName.setText(song.songName);
         viewHolder.authorName.setText(song.authorName);
         viewHolder.pickitsCounter.setText(String.valueOf(song.picksCount));
+        viewHolder.position.setText(String.valueOf(position + 1));
 
         return convertView;
     }
--- app/src/main/java/pickit/com/pickit/UI/Screens/HomeFragment.java
+++ app/src/main/java/pickit/com/pickit/UI/Screens/HomeFragment.java
@@ -22,15 +22,17 @@ import pickit.com.pickit.R;
 
 public class HomeFragment extends Fragment {
 
+    //Parameters:
     public static final String TAG = "HomeFragment";
 
+    /**
+     * creating new instance of HomeFragment
+     * @return the instance of Home Fragment.
+     */
     public static HomeFragment newInstance() {
-
         Bundle args = new Bundle();
-
         HomeFragment fragment = new HomeFragment();
         fragment.setArguments(args);
-
         return fragment;
     }
 
@@ -39,30 +41,70 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         List<PISong> songList = new ArrayList<PISong>();
+        //************ only for test******************
         PISong song = new PISong();
-        song.songName = "or hagever";
+        song.songName = "tal hagever";
         song.authorName = "yotal";
         song.songId = 1;
         song.picksCount = 67;
 
+        PISong song2 = new PISong();
+        song2.songName = "tal hagever";
+        song2.authorName = "yotal";
+        song2.songId = 2;
+        song2.picksCount = 67;
+
+        PISong song3 = new PISong();
+        song3.songName = "yotam hagever";
+        song3.authorName = "yotal";
+        song3.songId = 3;
+        song3.picksCount = 67;
+
+        PISong song4 = new PISong();
+        song4.songName = "yossi hagever";
+        song4.authorName = "yotal";
+        song4.songId = 4;
+        song4.picksCount = 67;
+
+        PISong song5 = new PISong();
+        song5.songName = "shomob hagever";
+        song5.authorName = "yotal";
+        song5.songId = 5;
+        song5.picksCount = 67;
+
+        PISong song6 = new PISong();
+        song6.songName = "hhhh hagever";
+        song6.authorName = "yotal";
+        song6.songId = 6;
+        song6.picksCount = 67;
+
+        PISong song7 = new PISong();
+        song7.songName = "oaaar hagever";
+        song7.authorName = "yotal";
+        song7.songId = 7;
+        song7.picksCount = 67;
+
         songList.add(song);
+        songList.add(song2);
+        songList.add(song3);
+        songList.add(song4);
+        songList.add(song5);
+        songList.add(song6);
+        songList.add(song7);
+        //*******************************
 
-        View view = inflater.from(getContext()).inflate(R.layout.fragment_home, container, false);
-        ListView list = (ListView)view.findViewById(R.id.songList);
-        PISongsListAdapter adapter = new PISongsListAdapter(getContext(), R.layout.pi_songs_list_cell);
-        adapter.setSongsList(songList);
-        list.setAdapter(adapter);
+        View homeFragmentView = inflater.from(getContext()).inflate(R.layout.fragment_home, container, false);
+        ListView songsTableListView = (ListView)homeFragmentView.findViewById(R.id.songList);
+        PISongsListAdapter listAdapter = new PISongsListAdapter(getContext(), R.layout.pi_songs_list_cell);
+        listAdapter.setSongsList(songList);
+        songsTableListView.setAdapter(listAdapter);
 
-        return view;
-
+        return homeFragmentView;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-
-
     }
 
 
